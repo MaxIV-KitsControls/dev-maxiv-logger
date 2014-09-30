601b1c377ecf21bae65d62b5d54b020cd9c0b4fe
Johan Forsberg <johan.forsberg@maxlab.lu.se>
2014-09-30 10:56:30 +0200

    rename

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 from setuptools import setup
 
-setup(name = "python-loggerds",
-      version = "0.1.0",
+setup(name = "tangods-logger",
+      version = "0.2.0",
       description = "Logger device which logs stuff to Elasticsearch",
       packages = ['loggerds'],
       scripts = ['scripts/loggerds']
