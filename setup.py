569e09a7f24ae2db8207f9278a9416c212de7689
Johan Forsberg <johan.forsberg@maxlab.lu.se>
2014-11-20 14:23:37 +0100

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name = "tangods-logger",
-      version = "0.4.0",
+      version = "0.4.1",
       description = "Logger device which logs stuff to Elasticsearch",
       packages = ['loggerds'],
       scripts = ['scripts/loggerds']
