e9b36b0223c756cee65d04bd523fe52b016935d9
Johan Forsberg <johan.forsberg@gmail.com>
2017-05-11 13:42:20 +0200

    v1.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name = "tangods-logger",
-      version = "1.0.1",
+      version = "1.0.2",
       description = "Logger device which logs stuff to Elasticsearch",
       packages = ['loggerds'],
       scripts = ['scripts/loggerds']
