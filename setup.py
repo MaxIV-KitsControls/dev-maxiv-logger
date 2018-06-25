0bd49b8489fb1ddb651b1adeab659bae1ba9c345
Vasileios Martos <Vasileios.Martos@maxiv.lu.se>
2018-06-25 11:56:15 +0200

    add dependencies, bump

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@
 from setuptools import setup
 
 setup(name = "tangods-logger",
-      version = "1.0.2",
+      version = "1.6.2",
       description = "Logger device which logs stuff to Elasticsearch",
       packages = ['loggerds'],
-      scripts = ['scripts/loggerds']
+      scripts = ['scripts/loggerds'],
+      install_requires=['setuptools', 'elasticsearch>=6.2.0']
 )
